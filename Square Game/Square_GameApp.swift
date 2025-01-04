52ab174726832ffa7742f19065a4c756af5cfad5
Isurunath99 <isurunathnanayakkara@gmail.com>
2025-01-04 20:56:47 +0530

    Initial Commit

---
+++ Square Game/Square_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  Square_GameApp.swift
+//  Square Game
+//
+//  Created by sasiri rukshan nanayakkara on 1/4/25.
+//
+
+import SwiftUI
+
+@main
+struct Square_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Square GameTests/Square_GameTests.swift
@@ -0,0 +1,17 @@
+//
+//  Square_GameTests.swift
+//  Square GameTests
+//
+//  Created by sasiri rukshan nanayakkara on 1/4/25.
+//
+
+import Testing
+@testable import Square_Game
+
+struct Square_GameTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
